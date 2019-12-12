71ce34069aee6e0b807c7092e1fb020ddeddfb65
SGATADE <sgtechwork@gmail.com>
2019-12-12 15:54:53 +0100

    Fixed onChange error in Panel.js

--- src/provider/Panel.js
+++ src/provider/Panel.js
@@ -1,72 +1,70 @@
 import React from "react";
-import "./Panel.css"
+import "./Panel.css";
 
 class Panel extends React.Component {
 
+    changeOptions = () => {
+        console.log("Changing options!");
+    }
+
     render() {
         const fxSearchBox = {
-            "height": "36px",
+            height: "36px",
             "border-radius": "6px"
-        }
+        };
 
         return (
             <React.Fragment>
-            <div className="four wide column" style={{"paddingLeft": "22px"}}>
-                {/*  */}
+                <div className="four wide column" style={{ paddingLeft: "22px" }}>
+                    {/*  */}
 
-                {/* <!-- Search Subscribers --> */}
+                    {/* <!-- Search Subscribers --> */}
 
-                <div className="ui category search">
-                    <div className="ui fluid icon input">
-                        <input
-                            className={fxSearchBox}
-                            type="text"
-                            placeholder="Search anything..."
-                        />
-                        <i className="search icon" ></i>
+                    <div className="ui category search">
+                        <div className="ui fluid icon input">
+                            <input className={fxSearchBox} type="text" placeholder="Search anything..." />
+                            <i className="search icon"></i>
+                        </div>
+                        <div className="results"></div>
                     </div>
-                    <div className="results"></div>
-                </div>
 
-                {/* <!-- Options --> */}
+                    {/* <!-- Options --> */}
 
-                <h5 className="ui top attached header" style={{"marginTop": "14px"}}>
-                    Options
-                </h5>
-                <div className="ui attached segment">
-                    <div className="ui form">
-                        <div className="grouped fields">
-                            <div className="field">
-                                <div className="ui radio checkbox">
-                                    <input type="radio" name="example2" checked="checked" onChange="test" />
-                                    <label>All Subscribers</label>
+                    <h5 className="ui top attached header" style={{ marginTop: "14px" }}>
+                        Options
+                    </h5>
+                    <div className="ui attached segment">
+                        <div className="ui form">
+                            <div className="grouped fields">
+                                <div className="field">
+                                    <div className="ui radio checkbox">
+                                        <input type="radio" name="example2" checked="checked" onChange={this.changeOptions} />
+                                        <label>All Subscribers</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="field">
-                                <div className="ui radio checkbox">
-                                    <input type="radio" name="example2" onChange="test" />
-                                    <label>On-boarded Subscribers</label>
+                                <div className="field">
+                                    <div className="ui radio checkbox">
+                                        <input type="radio" name="example2" onChange={this.changeOptions} />
+                                        <label>On-boarded Subscribers</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="field">
-                                <div className="ui radio checkbox">
-                                    <input type="radio" name="example2" onChange="test" />
-                                    <label>All Subscribers</label>
+                                <div className="field">
+                                    <div className="ui radio checkbox">
+                                        <input type="radio" name="example2" onChange={this.changeOptions} />
+                                        <label>All Subscribers</label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <h5 className="ui attached header">Filter</h5>
+                    <div className="ui bottom attached segment">
+                        <p>Humans don't think of lions as being like cats, but they are.</p>
+                    </div>
                 </div>
-                <h5 className="ui attached header">
-                    Filter
-                </h5>
-                <div className="ui bottom attached segment">
-                    <p>Humans don't think of lions as being like cats, but they are.</p>
-                </div>
-            </div>
             </React.Fragment>
         );
     }
-};
+}
 
 export default Panel;
